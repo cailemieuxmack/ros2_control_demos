2b1a1b6d5869c62f0146c4379a5746f6c1cf0ccf
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-03-31 10:40:44 -0500

    put code where it actually should be

--- example_7/controller/r6bot_controller.cpp
+++ example_7/controller/r6bot_controller.cpp
@@ -190,53 +190,50 @@ controller_interface::return_type RobotController::update(
     new_msg_ = false;
   }
 
-  // DEBUG
-  // Pass in the index and state
-  tmp_state->idx = myIdx; //.store(5);//myIdx;
-  //tmp_state->values[0] = tmp_vote->values[0]; // FIXME -> need to actually pass in the real state *******
-  serialize_joint_trajectory(trajectory_msg_, tmp_state->value);
-
-  // Actually store the state in the maped memory
-  for(int i = 0; i < 5; i++){
-    state_vote->value = tmp_state->value;
-  }
-  // Store the index last
-  state_vote->idx = tmp_state->idx;
-
-  // Sleep so that the controller can run
-  rclcpp::sleep_for(std::chrono::nanoseconds(100));
-
-  // Get the proposed values
-  tmp_vote->idx = data0->idx; 
-  tmp_vote->values[0] = data0->values[0];
-  //printf("idx: %d   value: %f\n", tmp_vote->idx, tmp_vote->values[0]);
-  //printf("read: %d,   %f\n", tmp_vote->idx, tmp_vote->values[0]);
-
-  std::cout << "idx recieved: " << tmp_vote->idx << std::endl;
-
-  //if (tmp_vote->idx.load() >= myIdx) {
-  if (tmp_vote->idx >= myIdx) {
-    // We have a new message
-    std::cout << "got: " << tmp_vote->values[0] << std::endl;
-    // std::cout << sizeof(trajectory_msg_) << std::endl;
-
-    // update index index
-    myIdx = tmp_vote->idx; //.load();
-    myIdx++;
-  } else {
-    // did not vote in time...
-  }
-
-  //std::cout << "trajectory_msg_ size: " << sizeof(trajectory_msg_) << std::endl;
-
-  // std::cout << "end" << std::endl;
-
-
-
-  // END DEBUG
-
   if (trajectory_msg_ != nullptr)
   {
+
+    // DEBUG
+    // Pass in the index and state
+    tmp_state->idx = myIdx; //.store(5);//myIdx;
+    //tmp_state->values[0] = tmp_vote->values[0]; // FIXME -> need to actually pass in the real state *******
+    serialize_joint_trajectory(trajectory_msg_, tmp_state->value);
+
+    // Actually store the state in the maped memory
+    for(int i = 0; i < 5; i++){
+      state_vote->value = tmp_state->value;
+    }
+    // Store the index last
+    state_vote->idx = tmp_state->idx;
+
+    // Sleep so that the controller can run
+    rclcpp::sleep_for(std::chrono::nanoseconds(100));
+
+    // Get the proposed values
+    tmp_vote->idx = data0->idx; 
+    tmp_vote->values[0] = data0->values[0];
+    //printf("idx: %d   value: %f\n", tmp_vote->idx, tmp_vote->values[0]);
+    //printf("read: %d,   %f\n", tmp_vote->idx, tmp_vote->values[0]);
+
+    std::cout << "idx recieved: " << tmp_vote->idx << std::endl;
+
+    //if (tmp_vote->idx.load() >= myIdx) {
+    if (tmp_vote->idx >= myIdx) {
+      // We have a new message
+      std::cout << "got: " << tmp_vote->values[0] << std::endl;
+      // std::cout << sizeof(trajectory_msg_) << std::endl;
+
+      // update index index
+      myIdx = tmp_vote->idx; //.load();
+      myIdx++;
+    } else {
+      // did not vote in time...
+    }
+
+    // std::cout << "end" << std::endl;
+    // END DEBUG
+
+
     interpolate_trajectory_point(*trajectory_msg_, time - start_time_, point_interp_);
     for (size_t i = 0; i < joint_position_command_interface_.size(); i++)
     {
