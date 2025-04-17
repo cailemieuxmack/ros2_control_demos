83cc51df56fd928607aec15e3802a1f8985b8694
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-04-16 20:13:17 -0500

    chatcpt don't fail me now...

--- example_7/controller/r6bot_controller.cpp
+++ example_7/controller/r6bot_controller.cpp
@@ -373,23 +373,25 @@ void RobotController::serialize_joint_trajectory(const std::shared_ptr<trajector
   for (size_t i = 0; i < src->points.size(); ++i) {
     // std::cout << "serialize_joint_trajectory: Inside main for loop" << std::endl;
       const auto& point = src->points[i];
+
+      MappedJointTrajectoryPoint* mapped_point = &state_vote->value.points[i];
       //auto& mapped_point = dest->value.points[i];
 
 
 
       std::cout << "serialize_joint_trajectory - inside loop: " << i << " - " << &dest->value.points[i] << std::endl;
 
-      dest->value.points[i].positions_length = point.positions.size();
+      mapped_point->positions_length = point.positions.size();
       for (size_t j = 0; j < point.positions.size(); ++j) {
           std::cout << "serialize_joint_trajectory - position: " << j << " - " << point.positions[j] << std::endl;
-          dest->value.points[i].positions[j] = point.positions[j];
-          std::cout << "mapped position: " << j << " - " << dest->value.points[i].positions[j] << std::endl;
+          mapped_point->positions[j] = point.positions[j];
+          std::cout << "mapped position: " << j << " - " << mapped_point->positions[j] << std::endl;
       }
 
-      dest->value.points[i].velocities_length = point.velocities.size();
+      mapped_point->velocities_length = point.velocities.size();
       for (size_t j = 0; j < point.velocities.size(); ++j) {
           std::cout << "serialize_joint_trajectory - velo: " << point.velocities[j] << std::endl;
-          dest->value.points[i].velocities[j] = point.velocities[j];
+          mapped_point->velocities[j] = point.velocities[j];
       }
 
       // mapped_point.accelerations_length = point.accelerations.size();
