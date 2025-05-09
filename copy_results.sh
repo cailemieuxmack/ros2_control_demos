a91513bccf911d81c6388dd758c82f76e22bbdf7
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-05-09 14:15:10 -0500

    fix script to make results direcory if needed and fix typo

--- copy_results.sh
+++ copy_results.sh
@@ -2,6 +2,11 @@
 
 curIdx=$1
 
+if [ ! -d "results" ]; then
+    mkdir results
+    echo "Results directory created."
+fi
+
 cp _state ./results/state_$curIdx
 
-cp _actuation ./results/acttuation_$curIdx
+cp _actuation ./results/actuation_$curIdx
