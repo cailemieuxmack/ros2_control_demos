5394eaaaa2164a88f3d73649e779c1ab661b4e22
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-05-12 10:31:22 -0500

    getting there

--- make_testcases.py
+++ make_testcases.py
@@ -1,22 +1,38 @@
 import csv
 import sys
 import random
+import os
 
-LINE_LEN = 7
+# LINE_LEN = 7
+REPAIR_PATH = "."
 MIN_IDX = 10
 
 # pass in the controller number
 bad = sys.argv[1]
 
-# max_idx = int(sys.argv[2])
+max_idx = int(sys.argv[2])
 
 with open("missed_" + bad + ".txt") as missed:
     miss_lines = missed.readlines()
     miss = []
     for x in miss_lines:
-        idx = x[:-1]
-        if int(idx) >= MIN_IDX:
+        idx = int(x[:-1])
+        if idx >= MIN_IDX:
             miss.append(idx)
 
     # miss = [x[:-1] for x in miss_lines]
     print("controller missed: " + str(miss))
+
+
+potential_passes = range(MIN_IDX, max_idx)
+
+passes = [x for x in potential_passes if x not in miss]
+
+print(f"passes: {passes}")
+
+
+miss_cases = random.sample(miss, 2)
+
+for i in miss:
+    os.system(f"cp state_{i} {REPAIR_PATH}/docker/test/n{i}")
+    os.system(f"cp actuation_{i} {REPAIR_PATH}/docker/test/output.n{i}")
