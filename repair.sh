9c4aa541bed7c45b36d2ff1e0b42417c9f27c6c7
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-05-15 14:40:37 -0500

    slowly but surely

--- repair.sh
+++ repair.sh
@@ -7,12 +7,12 @@ max_idx=$2
 
 here_dir=$(pwd)
 
-cp c$bad_num/controller.c docker/controller_vulnerable.c
+cp c$bad_num/controller.c docker/controller.c
 
 python3 make_testcases.py $1 $max_idx
 
-cp -r docker/ repair_$bad_num/
-cp repair.yml repair_$bad_num/
+# cp -r docker/ repair_$bad_num/
+# cp repair.yml repair_$bad_num/
 
 make
 
