83f791d7f9f87fc13d026d139d2395c9a2523e82
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-05-24 10:59:42 -0500

    test first vuln

--- example_7/controller/c0/controller.c
+++ example_7/controller/c0/controller.c
@@ -22,17 +22,13 @@ void interpolate_point(
   {
     for (size_t i = 0; i < point_1.positions_length; i++)
     {
-      point_interp->positions[i] = delta * point_2.positions[i] + (1.0 - delta) * point_1.positions[i];
+      // point_interp->positions[i] = delta * point_2.positions[i] + (1.0 - delta) * point_1.positions[i];
+      point_interp->positions[i] = (point_1.positions[i] + point_2.positions[i]) / 2;
     }
     for (size_t i = 0; i < point_1.positions_length; i++)
     {
-      point_interp->velocities[i] =
-        delta * point_2.velocities[i] + (1.0 - delta) * point_1.velocities[i];
-
-        // DEBUG This is the bug
-        
-        point_interp->velocities[i] = -1 * (point_interp->velocities[i] + 10);
-
+      // point_interp->velocities[i] = delta * point_2.velocities[i] + (1.0 - delta) * point_1.velocities[i];
+      point_interp->velocities[i] = (point_1.velocities[i] + point_2.velocities[i]) / 2;
     }
   }
   
